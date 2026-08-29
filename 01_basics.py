ab359e6d5644d903e29ae75607409f8f0b5891ff
Trinayan Bhuyan <trinayanbhuyan12@gmail.com>
2026-08-29 21:22:32 +0530

    Added basics and variables lesson

---
+++ 01_basics.py
@@ -0,0 +1,16 @@
+#My first Python lesson
+print("Hello, World!")
+print("I am learning Python step by step.")
+
+#Working with numbers
+print(10 + 5)
+print(20 - 4)
+print(6 * 3)
+
+#Using Variables
+item = "Notebook"
+price = 45
+quantity = 3 
+total = price * quantity
+print("Item Name:", item)
+print("Total Cost:", total)
