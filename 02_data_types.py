119669fe3fd2c435ec1cf2186a3e50955e2bb586
Trinayan Bhuyan <trinayanbhuyan12@gmail.com>
2026-08-30 22:03:09 +0530

    Added data types lesson

---
+++ 02_data_types.py
@@ -0,0 +1,8 @@
+#Lesson 2: Data Types
+title = "Pressure Sensor"
+reading_count = 5
+pressure_val = 14.7
+
+print(title, "is of type:", type(title))
+print(reading_count, "is of type:", type(reading_count))
+print(pressure_val, "is of type:", type(pressure_val))
