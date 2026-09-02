d13b52e1734bedadd1c37cf6cfbd569bbfd092a8
Trinayan Bhuyan <trinayanbhuyan12@gmail.com>
2026-09-02 23:50:15 +0530

    Added conditionals lesson

---
+++ 05_conditionals.py
@@ -0,0 +1,7 @@
+# Lesson 5: Decision Making (if/else)
+
+temperature = float(input("Enter boiler temperature (℃)"))
+if temperature > 100:
+    print("Warning: Temperature exceeds boiling point!")
+else:
+    print("Status normal: Temperature within safe limit.")
